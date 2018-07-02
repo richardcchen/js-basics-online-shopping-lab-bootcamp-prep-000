facd7fdd6154de5a2870843202b34025b885ea1b
Richard Chen <richard.c.chen@gmail.com>
2018-07-02 23:48:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  itemPrice = Math.floor(Math.random() * Math.floor(100));
+  let itemPrice = Math.floor(Math.random() * Math.floor(100));
   let objitem = {"itemName:": item, "itemPrice:": itemPrice};
   cart.push(objitem);
   return `${item} has been added to your cart.`
