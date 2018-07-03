c1390e12732c0f9460b9a20388fa96e633627735
Richard Chen <richard.c.chen@gmail.com>
2018-07-03 00:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function viewCart() {
 
 function total() {
   // write your code here
-  var carttotal;
+  let carttotal = 0;
   for (let i = 0; i < cart.length; i++){
     carttotal = carttotal + cart[i].itemPrice;
   }
