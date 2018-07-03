9af382dd77ee32e72474e1d80d2d42f46736fa11
Richard Chen <richard.c.chen@gmail.com>
2018-07-03 00:25:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var cartstring = "In your cart, you have ";
+    return cartstring;
     
   }
 }
