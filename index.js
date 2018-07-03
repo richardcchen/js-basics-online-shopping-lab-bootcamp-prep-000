23017afafc6e21b944f6b1d0dc3716e3ec68d2da
Richard Chen <richard.c.chen@gmail.com>
2018-07-03 01:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
-      cart.splice(i);
+      cart.splice(i,1);
       return cart;
     }
   }
