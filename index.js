2fd49539218fa235c6021af06e65930d166c5c1b
Richard Chen <richard.c.chen@gmail.com>
2018-07-03 00:47:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,9 +48,11 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++){
     if (item === cart[i].itemName){
-      cart
-    }
+      cart.splice(i);
+      return cart;
+    } 
   }
+  return "That item is not in your cart";
 }
 
 function placeOrder(cardNumber) {
