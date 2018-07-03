40500931ffd7fc6cc0a065668db0a2fee0bbed71
Richard Chen <richard.c.chen@gmail.com>
2018-07-03 00:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,6 +46,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (let i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName){
+      
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
